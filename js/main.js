3a97e3f7c01aa5e8a72526b6707a909e85c1a089
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-08-28 00:30:01 -0500

    Update 2015-08-28T05:30:00.039Z

--- js/main.js
+++ js/main.js
@@ -1,3 +1,24 @@
-'use strict';
+"use strict";
 
-angular.module('bach', []);
+// angular
+//   .module('bach', ['ngRoute'])
+//   .config(function ($routeProvider) {
+//     $routeProvider
+//       .when('/', {
+//         templateUrl: '/templates/landing/landing.html',
+//         controller: 'landingCtrl'
+//       })
+//       .when('/about', {
+//         templateUrl: '/templates/about/about.html',
+//         controller: 'aboutCtrl'
+//       })
+//       // .when('/', {
+//       //   templateUrl: '',
+//       //   controller: ''
+//       // })
+//   })
+//   .controller('landingCtrl', function ($scope) {
+//   })
+//   .controller('aboutCtrl', function (){
+
+//   })
