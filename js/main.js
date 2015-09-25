b3ad03f7f4d6886f7939d148c152a8375613b610
Lucas McDaniel <ldmcdaniel@gmail.com>
2015-09-25 02:57:58 -0500

    Update 2015-09-25T07:57:56.913Z

--- js/main.js
+++ js/main.js
@@ -1,24 +1,16 @@
-"use strict";
+'use strict';
 
-// angular
-//   .module('bach', ['ngRoute'])
-//   .config(function ($routeProvider) {
-//     $routeProvider
-//       .when('/', {
-//         templateUrl: '/templates/landing/landing.html',
-//         controller: 'landingCtrl'
-//       })
-//       .when('/about', {
-//         templateUrl: '/templates/about/about.html',
-//         controller: 'aboutCtrl'
-//       })
-//       // .when('/', {
-//       //   templateUrl: '',
-//       //   controller: ''
-//       // })
-//   })
-//   .controller('landingCtrl', function ($scope) {
-//   })
-//   .controller('aboutCtrl', function (){
+$(document).foundation();
 
-//   })
+angular.module('hobbitHole', ['ngRoute']).config(function ($routeProvider) {
+  $routeProvider.when('/', {
+    templateUrl: '/templates/landing/landing.html',
+    controller: 'landingCtrl'
+  }).when('/skills', {
+    templateUrl: '/templates/skills/skills.html',
+    controller: 'skillsCtrl'
+  }).when('/projects', {
+    templateUrl: '/templates/projects/projects.html',
+    controller: 'projectsCtrl'
+  });
+}).controller('landingCtrl', function ($scope) {}).controller('skillsCtrl', function () {}).controller('projectsCtrl', function () {});
