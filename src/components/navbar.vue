e8c0be2c2f8ad560712099075d0c17036273ec16
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2017-08-29 23:22:05 -0500

    Made sure all links are working

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -42,57 +42,6 @@
             <a href="/#/blog">
               Blog
             </a>
-            <div id="blogDropdown" class="navbar-dropdown is-boxed" data-style="width: 18rem;">
-                <a class="navbar-item" href="/2017/08/03/list-of-tags/">
-                  <div class="navbar-content">
-                    <p>
-                      <small class="has-text-info">03 Aug 2017</small>
-                    </p>
-                    <p>New feature: list of tags</p>
-                  </div>
-                </a>
-                <a class="navbar-item" href="/2017/08/01/bulma-bootstrap-comparison/">
-                  <div class="navbar-content">
-                    <p>
-                      <small class="has-text-info">01 Aug 2017</small>
-                    </p>
-                    <p>Bulma / Bootstrap comparison</p>
-                  </div>
-                </a>
-                <a class="navbar-item" href="/2017/07/24/access-previous-bulma-versions/">
-                  <div class="navbar-content">
-                    <p>
-                      <small class="has-text-info">24 Jul 2017</small>
-                    </p>
-                    <p>Access previous Bulma versions</p>
-                  </div>
-                </a>
-              <a class="navbar-item" href="http://bulma.io/blog/">
-                More posts
-              </a>
-              <hr class="navbar-divider">
-              <div class="navbar-item">
-                <div class="navbar-content">
-                  <div class="level is-mobile">
-                    <div class="level-left">
-                      <div class="level-item">
-                        <strong>Stay up to date!</strong>
-                      </div>
-                    </div>
-                    <div class="level-right">
-                      <div class="level-item">
-                        <a class="button bd-is-rss is-small" href="http://bulma.io/atom.xml">
-                          <span class="icon is-small">
-                            <i class="fa fa-rss"></i>
-                          </span>
-                          <span>Subscribe</span>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
           </div>
           <div class="navbar-item has-dropdown is-hoverable">
             <div class="navbar-link">
@@ -220,9 +169,9 @@
                 <a class="bd-tw-button button"
                     data-social-network="Twitter"
                     data-social-action="tweet"
-                    data-social-target="http://bulma.io"
+                    data-social-target="http://ldmcdaniel.com"
                     target="_blank"
-                    href="https://twitter.com/intent/tweet?text=Bulma: a modern CSS framework based on Flexbox&hashtags=bulmaio&url=http://bulma.io&via=jgthms">
+                    href="https://twitter.com/intent/tweet?text=Check out this great site by Lucas McDaniel:&url=http://ldmcdaniel.com">
                   <span class="icon">
                     <i class="fa fa-twitter"></i>
                   </span>
