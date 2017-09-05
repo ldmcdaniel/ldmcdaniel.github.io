559d85967c46987bfd4be3b2db1f17ef550a4dc2
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2017-09-05 00:17:03 -0500

    Added aside items and some JS

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,37 @@
 <template>
   <div class="home">
-    <div class="header">
-      <nav-bar></nav-bar>
-      <h1><span>Lucas </span><span>Mc</span><span>Daniel</span></h1>
+    <div class="columns">
+      <div v-bind:class="{ 'is-three-quarters': !isHidden }" class="column">
+        <section v-on:click="toggleSideNav">
+          <div class="header">
+            <nav-bar></nav-bar>
+            <h1><span>Lucas </span><span>Mc</span><span>Daniel</span></h1>
+          </div>
+          <div class="body">
+          </div>
+          <div class="header">
+            <h1><span>Full </span><span>Stack </span><span>Developer</span></h1>
+          </div>
+          <foot-bar></foot-bar>
+        </section>
+      </div>
+      <div v-bind:class="{ 'is-hidden': isHidden }" class="column">
+        <aside class="menu">
+          <ul class="menu-list">
+            <li><a>About Me</a></li>
+            <li><a>Blog</a></li>
+            <li><a>Projects</a>
+              <ul>
+                <li><a>Tank-U</a></li>
+                <li><a>Dotfiles</a></li>
+                <li><a>Calculator</a></li>
+                <li><a>Fitness</a></li>
+              </ul>
+            </li>
+          </ul>
+        </aside>
+      </div>
     </div>
-    <div class="body">
-    </div>
-    <div class="header">
-      <h1><span>Full </span><span>Stack </span><span>Developer</span></h1>
-    </div>
-    <foot-bar></foot-bar>
   </div>
 </template>
 
@@ -23,6 +45,7 @@ export default {
     return {
       msg: 'Full Stack Developer',
       showInstructions: false,
+      isHidden: true,
     };
   },
   methods: {
@@ -31,6 +54,9 @@ export default {
     },
     negateInstructionsValue() {
       this.showInstructions = !this.showInstructions;
+    },
+    toggleSideNav() {
+      this.isHidden = !this.isHidden;
     },
   },
   mounted() {
