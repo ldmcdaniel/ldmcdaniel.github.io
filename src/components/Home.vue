ddb47d61c7468f1e0b7fa8e8db11c0f173c3f5e0
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2017-09-10 12:25:45 -0500

    Added projects cards

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -3,32 +3,107 @@
     <div class="hero section-one">
       <h1><span>Lucas </span><span>Mc</span><span>Daniel</span></h1>
     </div>
-    <div class="body section-two">
-      <div class="topper"></div>
-      <div class="section-body">
-        <div class="columns">
-          <div class="column">
-            <div class="card">
+    <div class="section">
+      <div class="container is-fluid">
+        <div class="tabs is-toggle is-fullwidth is-large">
+          <ul>
+            <li v-on:click="revealElement('tanku')" v-bind:class="{ 'is-active': tanku }">
+              <a>
+                <span class="icon"><i class="fa fa-image"></i></span>
+                <span>Tank-U</span>
+              </a>
+            </li>
+            <li v-on:click="revealElement('dotfiles')" v-bind:class="{ 'is-active': dotfiles }">
+              <a>
+                <span class="icon"><i class="fa fa-music"></i></span>
+                <span>dotfiles</span>
+              </a>
+            </li>
+            <li v-on:click="revealElement('calculator')" v-bind:class="{ 'is-active': calculator }">
+              <a>
+                <span class="icon"><i class="fa fa-film"></i></span>
+                <span>Calculator</span>
+              </a>
+            </li>
+            <li v-on:click="revealElement('fitness')" v-bind:class="{ 'is-active': fitness }">
+              <a>
+                <span class="icon"><i class="fa fa-file-text-o"></i></span>
+                <span>Fitness</span>
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div class="body">
+          <div class="section-body">
+            <div class="card" v-show="tanku">
               <div class="card-header">
                 <div class="card-header-title">Tank-U</div>
-                <div class="card-header-icon"></div>
+                <div class="card-header-icon">
+                  <span class="icon has-text-info">
+                    <i class="fa fa-gamepad"></i>
+                  </span>
+                </div>
               </div>
+              <div class="card-image">
+                <figure class="image">
+                  <img src="../assets/Tank-U_Screenshot.png" alt="Placeholder image">
+                </figure>
+              </div>
+              <div class="content">Tank-U is a tower-defense game build with Phaser.js.</div>
             </div>
-            <h1>Hello World</h1>
-          </div>
-          <div class="column">
-            <h1>Hello World</h1>
+            <div class="card" v-show="dotfiles">
+              <div class="card-header">
+                <div class="card-header-title">dotfiles</div>
+                <div class="card-header-icon">
+                  <span class="icon has-text-info">
+                    <i class="fa fa-laptop"></i>
+                  </span>
+                </div>
+              </div>
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                </figure>
+              </div>
+              <div class="content">Dotfiles are a project I will work on for my whole career.</div>
+            </div>
+            <div class="card" v-show="calculator">
+              <div class="card-header">
+                <div class="card-header-title">Calculator</div>
+                <div class="card-header-icon">
+                  <span class="icon has-text-info">
+                    <i class="fa fa-calculator"></i>
+                  </span>
+                </div>
+              </div>
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                </figure>
+              </div>
+              <div class="content">A calculator app built for mobile with Ionic.</div>
+            </div>
+            <div class="card" v-show="fitness">
+              <div class="card-header">
+                <div class="card-header-title">Fitness</div>
+                <div class="card-header-icon">
+                  <span class="icon has-text-info">
+                    <i class="fa fa-hand-rock-o"></i>
+                  </span>
+                </div>
+              </div>
+              <div class="card-image">
+                <figure class="image is-4by3">
+                  <img src="http://bulma.io/images/placeholders/1280x960.png" alt="Placeholder image">
+                </figure>
+              </div>
+              <div class="content">A custom fitness application built with Vue.js.</div>
+            </div>
           </div>
         </div>
       </div>
-      <div class="section-footer"></div>
     </div>
-    <div class="panel section-three">
-      <h1><span>Full </span><span>Stack </span><span>Developer</span></h1>
-    </div>
-    <div class="body section-four">
-    </div>
-    <div class="panel section-five">
+    <div class="panel">
       <h1><span>Full </span><span>Stack </span><span>Developer</span></h1>
     </div>
   </div>
@@ -43,6 +118,10 @@ export default {
       msg: 'Full Stack Developer',
       showInstructions: false,
       isSideNavHidden: true,
+      tanku: true,
+      dotfiles: false,
+      calculator: false,
+      fitness: false,
     };
   },
   methods: {
@@ -51,6 +130,16 @@ export default {
     },
     negateInstructionsValue() {
       this.showInstructions = !this.showInstructions;
+    },
+    revealElement(element) {
+      const elements = ['tanku', 'dotfiles', 'calculator', 'fitness'];
+      elements.forEach((name) => {
+        if (name === element) {
+          this[name] = true;
+        } else {
+          this[name] = false;
+        }
+      });
     },
   },
   mounted() {
@@ -115,23 +204,23 @@ export default {
     }
   }
   .panel {
-    height: 100vh;
+    height: calc(100vh - 52px);
     background-image: url('../assets/Lucas_McDaniel_Alcatraz.jpg');
-    background-position: 43% 85%;
+    background-position: 46% 85%;
     background-size: cover;
   }
   .hero,
   .panel {
     position: relative;
   }
+  .footer {
+  }
   .body {
-    height: 75vh;
     .topper {
       height: 12vh;
       background-color: #ff0009;
     }
     .section-body {
-      height: 55vh;
       background-color: white;
     }
     .section-footer {
