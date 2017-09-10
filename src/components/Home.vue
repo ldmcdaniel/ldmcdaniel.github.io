05f4bfb89cf35c686ad1b0327cc99f7ad6c4f519
Lucas McDaniel <lucas.mcdaniel@smiledirectclub.com>
2017-09-10 01:57:33 -0500

    Added Hero image and panel image

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,34 @@
 <template>
-  <div class="home is-mobile">
-    <div class="header section-one">
+  <div class="home">
+    <div class="hero section-one">
       <h1><span>Lucas </span><span>Mc</span><span>Daniel</span></h1>
     </div>
     <div class="body section-two">
       <div class="topper"></div>
-      <div class="section-body"></div>
+      <div class="section-body">
+        <div class="columns">
+          <div class="column">
+            <div class="card">
+              <div class="card-header">
+                <div class="card-header-title">Tank-U</div>
+                <div class="card-header-icon"></div>
+              </div>
+            </div>
+            <h1>Hello World</h1>
+          </div>
+          <div class="column">
+            <h1>Hello World</h1>
+          </div>
+        </div>
+      </div>
       <div class="section-footer"></div>
     </div>
-    <div class="header section-three">
+    <div class="panel section-three">
       <h1><span>Full </span><span>Stack </span><span>Developer</span></h1>
     </div>
     <div class="body section-four">
     </div>
-    <div class="header section-five">
+    <div class="panel section-five">
       <h1><span>Full </span><span>Stack </span><span>Developer</span></h1>
     </div>
   </div>
@@ -66,18 +81,24 @@ export default {
   font-style: normal;
 }
 .home {
+  overflow-y: hidden;
   font-family: 'Bungee', cursive;
   text-align: center;
-  .header {
-    height: calc(100vh - 52px);
-    position: relative;
+  .hero {
+    @media screen and (orientation:portrait) {
+      height: calc(100vh - 52px);
+    }
+    height: calc(200vh - 52px);
+    background-image: url('../assets/Lucas_McDaniel_Milford_Sound_NZ.jpg');
+    background-position: top;
+    background-size: cover;
     h1 {
       font-weight: normal;
       font-size: 8rem;
       text-align: center;
       position: absolute;
       left: 50%;
-      top: 50%;
+      top: 16%;
       transform: translate(-50%, -50%);
       font-family: 'Bungee Shade', cursive;
       span {
@@ -93,17 +114,24 @@ export default {
       }
     }
   }
-  .body {
+  .panel {
     height: 100vh;
-    background-image: url('../assets/Hobbiton.jpeg');
-    background-position: center;
+    background-image: url('../assets/Lucas_McDaniel_Alcatraz.jpg');
+    background-position: 43% 85%;
     background-size: cover;
+  }
+  .hero,
+  .panel {
+    position: relative;
+  }
+  .body {
+    height: 75vh;
     .topper {
       height: 12vh;
       background-color: #ff0009;
     }
     .section-body {
-      height: 80vh;
+      height: 55vh;
       background-color: white;
     }
     .section-footer {
